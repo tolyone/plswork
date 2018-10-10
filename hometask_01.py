86736a635cf7a4b72cdca03adfafb41b4e89d9cb
toly <zecuter@gmail.com>
2018-10-10 21:39:19 +0300

    kk

---
+++ hometask_01.py
@@ -0,0 +1,46 @@
+import random
+import string
+
+# Задание 1
+print('Input string: ', end='')
+a = input()
+L = a.__len__()
+N = int(a.__len__()//5)
+n = 0
+
+for e in range(0, 5):
+    n = n + N
+    b = a[0:n]
+    c = a[n:]
+    a = b + '_' + c
+
+a = a.split('_')
+print(a)
+
+# Задание 2
+s = ''
+print("Input N=", end='')
+J = int(input())
+
+for e in range(1, J):
+    s = s + random.choice(string.ascii_letters + string.digits)
+    if random.randint(0, 5) > 3:
+        s = s + '_'
+
+s = s.split('_')
+print(s)
+
+
+def foo(d):
+    m = 0
+    print(d)
+    for k in d:
+        for p in e:
+            if 'A' <= p <= 'Z':
+                m = m + 1
+        if n > 0:
+            print(k)
+        m = 0
+
+
+foo(s)
