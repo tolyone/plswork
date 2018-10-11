6e8345bd6c30dee5bb7b16c7bab18fdfb4cee25d
tolyone <tolybook@gmaill.com>
2018-10-11 10:31:22 +0300

    123

--- hometask_01.py
+++ hometask_01.py
@@ -8,6 +8,17 @@ L = a.__len__()
 N = int(a.__len__()//5)
 n = 0
 
+def foo(d):
+    m = 0
+    print(d)
+    for k in d:
+        for p in k:
+            if 'A' <= p <= 'Z':
+                m = m + 1
+        if m > 0:
+            print(k)
+        m = 0
+
 for e in range(0, 5):
     n = n + N
     b = a[0:n]
@@ -16,6 +27,7 @@ for e in range(0, 5):
 
 a = a.split('_')
 print(a)
+foo(a)
 
 # Задание 2
 s = ''
@@ -29,18 +41,5 @@ for e in range(1, J):
 
 s = s.split('_')
 print(s)
-
-
-def foo(d):
-    m = 0
-    print(d)
-    for k in d:
-        for p in e:
-            if 'A' <= p <= 'Z':
-                m = m + 1
-        if n > 0:
-            print(k)
-        m = 0
-
 
 foo(s)
